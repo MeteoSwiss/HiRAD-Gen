3eb4d0a7b15af18b42ae647a2f08a1c5b45b5b8e
Petar Stamenkovic <petar.stamenkovic@meteoswiss.ch>
2025-05-09 14:22:55 +0200

    adapt sbatch script to slurm config

--- src/hirad/testrun.sh
+++ src/hirad/testrun.sh
@@ -5,20 +5,33 @@
 ### HARDWARE ###
 #SBATCH --partition=debug
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:4
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=16G
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=72
 #SBATCH --time=00:30:00
 #SBATCH --no-requeue
 #SBATCH --exclusive
 
 ### OUTPUT ###
-#SBATCH --output=/scratch/mch/pstamenk/logs/regression_test.log
-#SBATCH --error=/scratch/mch/pstamenk/logs/regression_test.err
+#SBATCH --output=/iopsstor/scratch/cscs/pstamenk/logs/regression_test.log
+#SBATCH --error=/iopsstor/scratch/cscs/pstamenk/logs/regression_test.err
+
+### ENVIRONMENT ####
+#SBATCH --uenv=pytorch/v2.6.0:/user-environment
+#SBATCH --view=default
+#SBATCH -A a-a01
 
 # Choose method to initialize dist in pythorch
-export DISTRIBUTED_INITIALIZATION_METHOD=ENV
+export DISTRIBUTED_INITIALIZATION_METHOD=SLURM
+
+MASTER_ADDR="$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)"
+echo "Master node : $MASTER_ADDR"
+# Get IP for hostname.
+MASTER_ADDR="$(getent ahosts "$MASTER_ADDR" | awk '{ print $1; exit }')"
+echo "Master address : $MASTER_ADDR"
+export MASTER_ADDR
+export MASTER_PORT=29500
+echo "Master port: $MASTER_PORT"
 
 # Get number of physical cores using Python
 PHYSICAL_CORES=$(python -c "import psutil; print(psutil.cpu_count(logical=False))")
@@ -27,15 +40,12 @@ LOCAL_PROCS=${SLURM_NTASKS_PER_NODE:-1}
 # Compute threads per process
 OMP_THREADS=$(( PHYSICAL_CORES / LOCAL_PROCS ))
 export OMP_NUM_THREADS=$OMP_THREADS
-echo "Node: $(hostname)"
 echo "Physical cores: $PHYSICAL_CORES"
 echo "Local processes: $LOCAL_PROCS"
 echo "Setting OMP_NUM_THREADS=$OMP_NUM_THREADS"
 
-# activate conda env
-CONDA_ENV=train
-source /users/pstamenk/.bashrc
-mamba activate $CONDA_ENV
-
 # python src/hirad/training/train.py --config-name=training_era_cosmo_testrun.yaml
-torchrun --nproc-per-node=$LOCAL_PROCS src/hirad/training/train.py --config-name=training_era_cosmo_regression.yaml
+srun bash -c "
+    . ./train_env/bin/activate
+    python src/hirad/training/train.py --config-name=training_era_cosmo_regression.yaml
+"
