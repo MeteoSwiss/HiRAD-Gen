c6e632a1bdc52d9f932baf15beab93abc187d94f
Mary McGlohon <Mary.McGlohon@meteoswiss.ch>
2025-05-12 18:39:35 +0200

    Adjust how reshaping is done before error calcs

--- src/hirad/eval/run_scoring.py
+++ src/hirad/eval/run_scoring.py
@@ -17,18 +17,25 @@ def main():
 
     target = torch.load(os.path.join(input_directory, 'cosmo', date), weights_only=False)
     baseline = torch.load(os.path.join(input_directory, 'era-interpolated', date), weights_only=False)
-    #prediction_file = torch.load(os.path.join(predictions_directory, date), weights_only=False)
-    prediction = torch.load(os.path.join(input_directory, 'cosmo', '20160101-0000'), weights_only=False)
+    prediction = torch.load(os.path.join(predictions_directory, date), weights_only=False)
     lat_lon = torch.load(os.path.join(input_directory, 'info', 'cosmo-lat-lon'), weights_only=False)
 
-    # Reshape grides to be the same as prediction
-    #target = target.squeeze().reshape(-1,*prediction.shape),
+    with open(os.path.join(input_directory), 'info', 'cosmo.yaml') as cosmo_file:
+        cosmo_config = yaml.safe_load(cosmo_file)
+    channels = cosmo_config['select']
+
+    # Reshape predictions, if necessary
+    # target is shape [channels, ensembles, points]
+    # prediction is shape [channels, ensembles, x, y]
+    prediction = prediction.reshape(*target.shape)
+
+    latitudes = lat_lon[:,0]
+    longitudes = lat_lon[:,1]
+    
+    # convert to torch
     target = torch.from_numpy(target)
     prediction = torch.from_numpy(prediction)
-    #prediction = prediction.squeeze().reshape(-1,*prediction.shape)
-    latitudes = lat_lon[:,0] #.squeeze().reshape(-1,*prediction.shape)
-    longitudes = lat_lon[:,1] #squeeze().reshape(-1,*prediction.shape)
-       
+
     errors = metrics.absolute_error(prediction[0,:,:], target[0,:,:])
     plotting.plot_error_projection(errors, latitudes, longitudes, os.path.join('plots/errors/', date))
 
