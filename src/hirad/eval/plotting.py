8ba0c5a21e21a09ff1b248221f01378e4bc30754
Mary McGlohon <Mary.McGlohon@meteoswiss.ch>
2025-05-12 18:38:19 +0200

    Plot absolute error onto a projection for a given date

---
+++ src/hirad/eval/plotting.py
@@ -0,0 +1,16 @@
+import logging
+
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_error_projection(values: np.array, latitudes: np.array, longitudes: np.array, filename: str):
+    fig = plt.figure()
+    fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
+    logging.info(f'plotting values to {filename}')
+    p = ax.scatter(x=longitudes, y=latitudes, c=values)
+    ax.coastlines()
+    ax.gridlines(draw_labels=True)
+    plt.colorbar(p, label="absolute error", orientation="horizontal")
+    plt.savefig(filename)
+    plt.close('all')
