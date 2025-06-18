f2d26a2f057b2b6d171e091a822af39c75b09af2
Mary McGlohon <Mary.McGlohon@meteoswiss.ch>
2025-06-18 12:20:19 +0200

    Add interpolation script

--- interpolate.sh
+++ interpolate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 conda init
-conda activate regridding
-python src/input_data/interpolate_basic.py src/input_data/era.yaml src/input_data/cosmo.yaml /store_new/mch/msopr/hirad-gen/basic-torch/trim_19_full/
+conda activate anemoi2
+#python src/input_data/interpolate_basic.py src/input_data/era-1h.yaml src/input_data/cosmo-1h.yaml /store_new/mch/msopr/hirad-gen/basic-torch/trim_19_1h/
+python src/input_data/interpolate_basic.py src/input_data/era-test.yaml src/input_data/cosmo-test.yaml /store_new/mch/msopr/hirad-gen/basic-torch/trim_19_thread/
