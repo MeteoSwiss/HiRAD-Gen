a284047ec4898cda2e91afd2ae99cfff4fd2548d
David Leutwyler <14977216+leuty@users.noreply.github.com>
2025-08-04 14:46:21 +0200

    Revert "New script to plot maps (#18)"
    This reverts commit 001d5e2b8e2f7ce5eabe48a43cccf7e4b461ce66.

--- src/hirad/eval/__init__.py
+++ src/hirad/eval/__init__.py
@@ -1,2 +1,2 @@
 from .metrics import absolute_error, compute_mae, average_power_spectrum, crps
-from .plotting import plot_map, plot_error_projection, plot_power_spectra, plot_scores_vs_t
+from .plotting import plot_error_projection, plot_power_spectra, plot_scores_vs_t
