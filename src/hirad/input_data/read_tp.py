630086a539a890170977f5d2689c21f6ebe085c0
Mary McGlohon <Mary.McGlohon@meteoswiss.ch>
2025-08-25 15:50:19 +0200

    Clean up copernicus processing

--- src/hirad/input_data/read_tp.py
+++ src/hirad/input_data/read_tp.py
@@ -1,5 +1,6 @@
 import logging
 import netCDF4
+import xarray
 from anemoi.datasets import open_dataset
 import numpy as np
 import yaml
@@ -8,6 +9,10 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from matplotlib.colors import BoundaryNorm, ListedColormap
+
+from hirad.eval.plotting import plot_map_precipitation, plot_scores_vs_t
+from hirad.eval.metrics import compute_mae, absolute_error
+
 
 import interpolate_basic
 
@@ -24,18 +29,20 @@ ANEMOI_6H_FILENAME = "/scratch/mch/apennino/data/aifs-ea-an-oper-0001-mars-n320-
 COSMO_6H_FILENAME = "/scratch/mch/fzanetta/data/anemoi/datasets/mch-co2-an-archive-0p02-2015-2020-6h-v3-pl13.zarr"
 COSMO_1H_FILENAME = "/scratch/mch/fzanetta/data/anemoi/datasets/mch-co2-an-archive-0p02-2015-2020-1h-v3-pl13.zarr"
 COSMO_CONFIG_FILE="src/input_data/cosmo.yaml"
-CDF_FILENAME = "8e49f064d738154bed136666ff72ae1c.nc"
+#CDF_FILENAME = "/store_new/mch/msopr/hirad-gen/copernicus-datasets/tp-janfeb2020.nc"
+CDF_FILENAME = "/store_new/mch/msopr/hirad-gen/copernicus-datasets/surface-janfeb2020.zip"
+GRIB_FILENAME = "/store_new/mch/msopr/hirad-gen/copernicus-datasets/surface-janfeb2020.grib"
 
 
 LAT = np.arange(-4.42, 3.36 + 0.02, 0.02)
 LON = np.arange(-6.82, 4.80 + 0.02, 0.02)
 RELAX_ZONE = 19 # Number of points dropped on each side (relaxation zone)
 
-
-def extract_values(netcdf_data):
+def extract_netcdf_values(netcdf_data):
     netcdf_lat = netcdf_data['latitude'][:]
     netcdf_lon = netcdf_data['longitude'][:]
     netcdf_tp = netcdf_data['tp'][:,:]
+    logging.info(f'num nonzeros in tp is {np.count_nonzero(netcdf_tp)}')
     values = np.zeros((netcdf_tp.shape[0], netcdf_tp.shape[1]*netcdf_tp.shape[2]))
     latitudes = np.zeros(values.shape[1])
     longitudes = np.zeros(values.shape[1])
@@ -44,111 +51,94 @@ def extract_values(netcdf_data):
         if i % 10 == 0:
             print(i)
         for j in range(len(netcdf_lon)):
-            grid_index = i * len(netcdf_lon) + j
-            values[:,grid_index] = netcdf_tp[:,i,j]
-            latitudes[grid_index] = netcdf_lat[i]
-            longitudes[grid_index] = netcdf_lon[j]
-    return values, latitudes, longitudes
-
-def plot_map(values: np.array,
-             filename: str,
-             label='',
-             title='',
-             vmin=None,
-             vmax=None,
-             cmap=None,
-             extend='neither',
-             norm=None,
-             ticks=None):
-    """Plot observed or interpolated data in a scatter plot."""
-    logging.info(f'Creating map: {filename}')
-
-    latitudes  = LAT[RELAX_ZONE : RELAX_ZONE + 352]
-    longitudes = LON[RELAX_ZONE : RELAX_ZONE + 544]
-    lon2d, lat2d = np.meshgrid(longitudes, latitudes)
-
-    fig, ax = plt.subplots(
-        figsize=(8, 6),
-        subplot_kw={"projection": ccrs.RotatedPole(pole_longitude=-170.0,
-                                                   pole_latitude=  43.0)}
-    )
-    values = values.reshape((len(latitudes), len(longitudes)))
-    contour = ax.pcolormesh(
-        lon2d, lat2d, values,
-        cmap=cmap, shading="auto",
-        norm=norm if norm else None,
-        vmin=None if norm else vmin,
-        vmax=None if norm else vmax,
-    )
-    ax.coastlines()
-    ax.add_feature(cfeature.BORDERS, linewidth=1)
-    ax.gridlines(visible=False)
-    ax.set_xticks([])
-    ax.set_yticks([])
-
-    plt.title(title)
-    cbar = plt.colorbar(
-        contour,
-        label=label,
-        orientation="horizontal",
-        extend=extend,
-        shrink=0.75,
-        pad=0.02
-    )
-    if ticks is not None:
-        cbar.set_ticks(ticks)
-        cbar.set_ticklabels([f'{tick:g}' for tick in ticks])
-
-    plt.tight_layout()
-    fig.savefig(f"{filename}.png", dpi=300, bbox_inches="tight")
-    plt.close(fig)
-
-def plot_map_precipitation(values, filename, title='', threshold=0.1, rfac=1000.0):
-    """Plot precipitation data with specific colormap and thresholds."""
-    # Scale and mask values below threshold
-    values = rfac * values # m/h --> mm/h
-    values = np.ma.masked_where(values <= threshold, values)
-
-    # Predefined colors and bounds specific for precipitation
-    colors = ['none', 'powderblue', 'dodgerblue', 'mediumblue',
-              'forestgreen', 'limegreen', 'lawngreen',
-              'yellow', 'gold', 'darkorange', 'red',
-              'darkviolet', 'violet', 'thistle']
-    bounds = [0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 30, 50, 70, 100, 150, 200]
-    bounds = [0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000]
-
-    cmap = ListedColormap(colors)
-    norm = BoundaryNorm(bounds, ncolors=len(colors), clip=False)
-
-    plot_map(
-        values, filename,
-        cmap=cmap,
-        norm=norm,
-        ticks=bounds,
-        title=title,
-        label='mm/h',
-        extend='max'
-    )
+           grid_index = i * len(netcdf_lon) + j
+           values[:,grid_index] = netcdf_tp[:,i,j]
+           latitudes[grid_index] = netcdf_lat[i]
+           longitudes[grid_index] = netcdf_lon[j]
+    reshape_values = np.reshape(netcdf_tp, (netcdf_tp.shape[0], netcdf_tp.shape[1]*netcdf_tp.shape[2]))
+    logging.info(f'Array equal? {np.array_equal(values, reshape_values)}')
+    return reshape_values, latitudes, longitudes
  
+def reshape_to_cosmo(vals):
+    return vals.reshape((len(LAT)-RELAX_ZONE*2, len(LON)-RELAX_ZONE*2))
 
-print(interpolate_basic.regrid)
-    
-file_id = netCDF4.Dataset(CDF_FILENAME)
-#anemoi1 = open_dataset(ANEMOI_1H_FILENAME)
-#anemoi6 = open_dataset(ANEMOI_6H_FILENAME)
-#with open(COSMO_CONFIG_FILE) as cosmo_file:
-#    cosmo_config = yaml.safe_load(cosmo_file)
-#cosmo = open_dataset(cosmo_config)
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+logging.info('loading data')
+grib_data = xarray.load_dataset(GRIB_FILENAME)
+netcdf_data = netCDF4.Dataset(CDF_FILENAME)
 cosmo1 = open_dataset(COSMO_1H_FILENAME, trim_edge=19, select=['tp'],start='2016-01-01',end='2016-02-29')
 cosmo6 = open_dataset(COSMO_6H_FILENAME, trim_edge=19, select=['tp'],start='2016-01-01',end='2016-02-29')
-
+era1 = open_dataset(ANEMOI_1H_FILENAME, select=['tp'],start='2016-01-01',end='2016-02-29')
+era6 = open_dataset(ANEMOI_6H_FILENAME, select=['tp'],start='2016-01-01',end='2016-02-29')
+logging.info('loading data complete')
 
 output_grid= np.column_stack((cosmo1.longitudes, cosmo1.latitudes))
-print(output_grid.shape)
-print(cosmo1[0,0,0,:].shape)
 
-plot_map_precipitation(values=cosmo1[0,:], filename="cosmo1.png")
-plot_map_precipitation(values=cosmo6[0,:], filename="cosmo6.png")
+logging.info('processing netcdf data')
+netcdf_values, netcdf_latitudes, netcdf_longitudes = extract_netcdf_values(netcdf_data=netcdf_data)
+netcdf_grid=np.column_stack((netcdf_longitudes, netcdf_latitudes))
+
+make_plots = False
+
+prev_netcdf_regrid = []
+
+netcdf_error = np.zeros(cosmo1.dates.shape)
+era_norm_error = np.zeros(cosmo1.dates.shape)
+netcdf_early_error = np.zeros(cosmo1.dates.shape)
+netcdf_late_error = np.zeros(cosmo1.dates.shape)
+
+for t in range(len(cosmo1.dates)):
+    date = cosmo1.dates[t]
+    era_date = era1.dates[t]
+    if date != era_date:
+        logging.error('dates do not match: cosmo date: {date}, era date: {era_date}')
+    if date != netcdf_data['valid_time'][t]:  
+        logging.error(f'dates do not match: cosmo date: {date}, netcdf: {netcdf_data["valid_time"][t]}')
+
+
+    # plot cosmo
+    if make_plots:
+        plot_map_precipitation(values=reshape_to_cosmo(cosmo1[t,:]), filename=f'plots/tp/{date}-cosmo1')
+        if t % 6 == 0:
+            if cosmo6.dates[t//6] != date:
+                logging.error(f'dates do not match: cosmo1: {date}, cosmo6: {cosmo6.dates[t//6]}')
+            plot_map_precipitation(values=reshape_to_cosmo(cosmo6[t//6,:]), filename=f'plots/tp/{date}-cosmo6')
+    
+    # plot netcdf
+    netcdf_vals = netcdf_values[t,:].reshape((1,1,netcdf_values.shape[1]))
+    netcdf_regrid=interpolate_basic.regrid(netcdf_vals, netcdf_grid, output_grid)
+    if make_plots:
+        plot_map_precipitation(reshape_to_cosmo(netcdf_regrid), f'plots/tp/{date}-netcdf')
+
+    # plot era
+    era_grid = np.column_stack((era1.longitudes, era1.latitudes))
+    era1_regrid = interpolate_basic.regrid(era1[t,:], era_grid, output_grid)
+    if make_plots:
+        plot_map_precipitation(reshape_to_cosmo(era1_regrid/6), f'plots/tp/{date}-era1-norm')
+
+        #if t % 6 == 0:
+        #    if era6.dates[t//6] != date:
+        #        logging.error(f'dates do not match: era1: {date}, era6: {era6.dates[t//6]}')
+        #    era6_regrid = interpolate_basic.regrid(era6[t//6,:], era_grid, output_grid)
+        #    plot_map_precipitation(reshape_to_cosmo(era6_regrid), f'plots/tp/{date}-era6')
+
+    era_norm_error[t] = np.mean(absolute_error(era1_regrid/6, cosmo1[t,:]))
+    netcdf_error[t] = np.mean(absolute_error(netcdf_regrid, cosmo1[t,:]))
+    logging.info(f'era norm error: {era_norm_error[t]} netcdf err: {netcdf_error[t]}')
+    if t>0:
+        netcdf_early_error[t] = np.mean(absolute_error(prev_netcdf_regrid, cosmo1[t,:]))
+        netcdf_late_error[t-1] = np.mean(absolute_error(netcdf_regrid, cosmo1[t-1,:]))
+        logging.info(f'netcdf early err: {netcdf_early_error[t]}, netcdf late err: {netcdf_late_error[t-1]}')
+    prev_netcdf_regrid = netcdf_regrid
+
+maes = {}
+maes['era normalized'] = era_norm_error
+maes['copernicus'] = netcdf_error
+maes['copernicus-early'] = netcdf_early_error
+maes['copernicus-late'] = netcdf_late_error
+plot_scores_vs_t(maes, times=cosmo1.dates, filename='plots/errors.png')
 
 #fig = plt.figure()
 #fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
@@ -161,23 +151,9 @@ plot_map_precipitation(values=cosmo6[0,:], filename="cosmo6.png")
 #fig.savefig('cosmo6.png')
 
 
-values, latitudes, longitudes = extract_values(netcdf_data=file_id)
-input_grid=np.column_stack((longitudes, latitudes))
-vals = values[0,:].reshape((1,1,values.shape[1]))
-regrid=interpolate_basic.regrid(vals, input_grid, output_grid)
-plot_map_precipitation(regrid, 'netcdf.png')
-
-
-era1 = open_dataset(ANEMOI_1H_FILENAME, select=['tp'],start='2016-01-01',end='2016-02-29')
-era6 = open_dataset(ANEMOI_6H_FILENAME, select=['tp'],start='2016-01-01',end='2016-02-29')
-era_grid = np.column_stack((era1.longitudes, era1.latitudes))
-era1_regrid = interpolate_basic.regrid(era1[0,:], era_grid, output_grid)
-plot_map_precipitation(era1_regrid, "era1.png")
 
 
 
-era6_regrid = interpolate_basic.regrid(era6[0,:], era_grid, output_grid)
-plot_map_precipitation(era1_regrid, "era6.png")
 
 
 
