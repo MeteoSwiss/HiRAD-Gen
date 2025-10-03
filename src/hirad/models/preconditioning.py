cfec8df2ced3e3de530e4851ab1c54678b0df681
Petar Stamenkovic <petar.stamenkovic@meteoswiss.ch>
2025-10-03 11:58:24 +0200

    update EDM SuperResolution model

--- src/hirad/models/preconditioning.py
+++ src/hirad/models/preconditioning.py
@@ -30,6 +30,8 @@ import torch.nn as nn
 
 from .meta import ModelMetaData
 
+from .utils import _wrapped_property
+
 network_module = importlib.import_module("hirad.models")
 
 
@@ -754,6 +756,14 @@ class EDMPrecondSuperResolution(nn.Module):
     arXiv preprint arXiv:2309.15214.
     """
 
+    # Classes that can be wrapped by this UNet class.
+    _wrapped_classes = {
+        "SongUNetPosEmbd",
+        "SongUNetPosLtEmbd",
+        "SongUNet",
+        "DhariwalUNet",
+    }
+
     def __init__(
         self,
         img_resolution: Union[int, Tuple[int, int]],
@@ -769,10 +779,17 @@ class EDMPrecondSuperResolution(nn.Module):
         **model_kwargs: dict,
     ):
         super().__init__() #meta=EDMPrecondSRMetaData
+
+        # Validation
+        if model_type not in self._wrapped_classes:
+            raise ValueError(
+                f"Model type '{model_type}' is not supported. "
+                f"Must be one of: {', '.join(self._wrapped_classes)}"
+            )
+        
         self.img_resolution = img_resolution
         self.img_in_channels = img_in_channels
         self.img_out_channels = img_out_channels
-        self.use_fp16 = use_fp16
         self.sigma_data = sigma_data
         self.sigma_min = sigma_min
         self.sigma_max = sigma_max
@@ -785,6 +802,45 @@ class EDMPrecondSuperResolution(nn.Module):
             **model_kwargs,
         )  # TODO needs better handling
         self.scaling_fn = self._scaling_fn
+        self.use_fp16 = use_fp16
+
+    @property
+    def use_fp16(self):
+        """
+        bool: Whether the model uses float16 precision.
+
+        Returns
+        -------
+        bool
+            True if the model is in float16 mode, False otherwise.
+        """
+        return self._use_fp16
+
+    @use_fp16.setter
+    def use_fp16(self, value: bool):
+        """
+        Set whether the model should use float16 precision.
+
+        Parameters
+        ----------
+        value : bool
+            If True, moves the model to torch.float16. If False, moves to torch.float32.
+
+        Raises
+        ------
+        ValueError
+            If `value` is not a boolean.
+        """
+        # NOTE: allow 0/1 values for older checkpoints
+        if not (isinstance(value, bool) or value in [0, 1]):
+            raise ValueError(
+                f"`use_fp16` must be a boolean, but got {type(value).__name__}."
+            )
+        self._use_fp16 = value
+        if value:
+            self.to(torch.float16)
+        else:
+            self.to(torch.float32)
 
     @staticmethod
     def _scaling_fn(
@@ -809,6 +865,18 @@ class EDMPrecondSuperResolution(nn.Module):
             Scaled and concatenated tensor of shape (B, C_in+C_out, H, W).
         """
         return torch.cat([c_in * x, img_lr.to(x.dtype)], dim=1)
+
+    # Properties delegated to the wrapped model
+    amp_mode = _wrapped_property(
+        "amp_mode",
+        "model",
+        "Set to ``True`` when using automatic mixed precision.",
+    )
+    profile_mode = _wrapped_property(
+        "profile_mode",
+        "model",
+        "Set to ``True`` to enable profiling of the wrapped model.",
+    )
 
     def forward(
         self,
@@ -906,29 +974,6 @@ class EDMPrecondSuperResolution(nn.Module):
         """
         return EDMPrecond.round_sigma(sigma)
 
-    @property
-    def amp_mode(self):
-        """
-        Return the *amp_mode* flag of the wrapped model or *None*.
-        """
-        return getattr(self.model, "amp_mode", None)
-
-    @amp_mode.setter
-    def amp_mode(self, value: bool):
-        """
-        Propagate *amp_mode* to the model and all its sub-modules.
-        """
-
-        if not isinstance(value, bool):
-            raise TypeError("amp_mode must be a boolean value.")
-
-        if hasattr(self.model, "amp_mode"):
-            self.model.amp_mode = value
-
-        for sub_module in self.model.modules():
-            if hasattr(sub_module, "amp_mode"):
-                sub_module.amp_mode = value
-
 # NOTE: This is a deprecated version of the EDMPrecondSuperResolution model.
 # This was used to maintain backwards compatibility and allow loading old models.
 @dataclass
@@ -976,6 +1021,8 @@ class EDMPrecondSR(EDMPrecondSuperResolution):
         Expected standard deviation of the training data, by default 0.5.
     model_type :str
         Class name of the underlying model, by default "SongUNetPosEmbd".
+    scale_cond_input : bool
+        Whether to scale the conditional input (deprecated), by default True.
     **model_kwargs : dict
         Keyword arguments for the underlying model.
 
@@ -1012,15 +1059,6 @@ class EDMPrecondSR(EDMPrecondSuperResolution):
             stacklevel=2,
         )
 
-        if scale_cond_input:
-            warnings.warn(
-                "scale_cond_input=True does not properly scale the conditional input. "
-                "(see https://github.com/NVIDIA/modulus/issues/229). "
-                "This setup will be deprecated. "
-                "Please set scale_cond_input=False.",
-                DeprecationWarning,
-            )
-
         super().__init__(
             img_resolution=img_resolution,
             img_in_channels=img_in_channels,
@@ -1033,9 +1071,47 @@ class EDMPrecondSR(EDMPrecondSuperResolution):
             **model_kwargs,
         )
 
+        if scale_cond_input:
+            warnings.warn(
+                "The `scale_cond_input=True` option does not properly scale the conditional input "
+                "and is deprecated. It is highly recommended to set `scale_cond_input=False`. "
+                "However, for loading a checkpoint previously trained with `scale_cond_input=True`, "
+                "this flag must be set to `True` to ensure compatibility. "
+                "For more details, see https://github.com/NVIDIA/modulus/issues/229.",
+                DeprecationWarning,
+            )
+            self.scaling_fn = self._legacy_scaling_fn
+
         # Store deprecated parameters for backward compatibility
         self.img_channels = img_channels
         self.scale_cond_input = scale_cond_input
+
+    @staticmethod
+    def _legacy_scaling_fn(
+        x: torch.Tensor, img_lr: torch.Tensor, c_in: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        This function does not properly scale the conditional input
+        (see https://github.com/NVIDIA/modulus/issues/229)
+        and will be deprecated.
+
+        Concatenate and scale the high-resolution and low-resolution tensors.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Noisy high-resolution image of shape (B, C_hr, H, W).
+        img_lr : torch.Tensor
+            Low-resolution image of shape (B, C_lr, H, W).
+        c_in : torch.Tensor
+            Scaling factor of shape (B, 1, 1, 1).
+
+        Returns
+        -------
+        torch.Tensor
+            Scaled and concatenated tensor of shape (B, C_in+C_out, H, W).
+        """
+        return c_in * torch.cat([x, img_lr.to(x.dtype)], dim=1)
 
     def forward(
         self,
