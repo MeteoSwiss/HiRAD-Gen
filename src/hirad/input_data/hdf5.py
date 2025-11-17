c34ea084fc40be23a5022db09f69f30c86e2c188
Mary McGlohon <Mary.McGlohon@meteoswiss.ch>
2025-11-17 19:34:52 +0100

    cleanup hdf5

--- src/hirad/input_data/hdf5.py
+++ src/hirad/input_data/hdf5.py
@@ -1,4 +1,4 @@
-import h5py
+from h5py import File
 import os
 import torch
 import logging
@@ -17,10 +17,10 @@ class HiradHdf5:
 		self.db_shape = tuple(shape)
 		self.to_write = to_write
 		if to_write:
-			self.dbf = h5py.File(DB_FILENAME, "w")
+			self.dbf = File(DB_FILENAME, "w")
 			self.dset = self.dbf.create_dataset("all-channels", self.db_shape, dtype='float64')
 		else:
-			self.dbf = h5py.File(DB_FILENAME, "r")
+			self.dbf = File(DB_FILENAME, "r")
 			self.dset = self.dbf.require_dataset("all-channels", self.db_shape, dtype='float64', exact=True)
 		return
 	
@@ -28,14 +28,12 @@ class HiradHdf5:
 		if not self.to_write:
 			raise PermissionError('database not opened with write')
 		logging.info('filling database')
-		with h5py.File(DB_FILENAME, "w") as dbf:
-			dset = dbf.create_dataset("all-channels", self.db_shape, dtype='float64')
-			#for i in range(len(self.files)):
-			for i in range(10):
-				f = self.files[i]
-				logging.info(f'reading {f}')
-				torchdata = torch.load(os.path.join(IN_DIR, f), weights_only=False)
-				dset[i,:] = torchdata
+		#for i in range(len(self.files)):
+		for i in range(10):
+			f = self.files[i]
+			logging.info(f'reading {f}')
+			torchdata = torch.load(os.path.join(IN_DIR, f), weights_only=False)
+			self.dset[i,:] = torchdata
 
 	def read_index(self, i: int):
 		return self.dset[i,:]
@@ -54,47 +52,18 @@ class HiradHdf5:
 			logging.info(nparray.dtype)
 			logging.info(np.equal(torchdata, nparray))
 	
-
-def fill_database():
-	print('starting')
-	files = sorted(os.listdir(IN_DIR))
-	with h5py.File(DB_FILENAME, "w") as dbf:
-		dset = dbf.create_dataset("all-channels", (len(files), 101, 1, 191488,), dtype='float64')
-		
-		for i in range(len(files)):
-		#for i in range(10):
-			f = files[i]
-			logging.info(f'reading {f}')
-			torchdata = torch.load(os.path.join(IN_DIR, f), weights_only=False)
-			dset[i,:] = torchdata
-
-
-def get_data_for_time(datefmt: str):
-	pass
-
-
-def read_database():
-	
-	files = sorted(os.listdir(IN_DIR))
-
-	with h5py.File(DB_FILENAME, "r") as dbf:
-		dset = dbf.require_dataset("all-channels", (len(files), 101, 1, 191488,), dtype='float64', exact=True)
-		for i in range(10):
-			nparray = dset[i,:]
-			f = files[i]
-			torchdata = torch.load(os.path.join(IN_DIR, f), weights_only=False)
-			logging.info(nparray.shape)
-			logging.info(nparray.dtype)
-			logging.info(np.equal(torchdata, nparray))
-	
-
 def main():
 	logging.basicConfig(
         format='%(asctime)s %(levelname)-8s %(message)s',
         level=logging.INFO,
         datefmt='%Y-%m-%d %H:%M:%S') 
+	# write DB
 	hdf5db = HiradHdf5(IN_DIR, DB_FILENAME, (101, 1, 191488), True)
+
+	hdf5db = HiradHdf5(IN_DIR, DB_FILENAME, (101, 1, 191488), False)
 	hdf5db.read_database()
+	hdf5db.read_index(0)
+	hdf5db.read_datetime('20160101-0900')
 	#read_database()
 	#nparray = get_data_for_time('20160101-0900')
 	#print(nparray.dtype)
