4c5bee5f411bd4aaf43606e7f0339c0eaa3ea5ce
Mary McGlohon <Mary.McGlohon@meteoswiss.ch>
2025-11-20 12:52:02 +0100

    add balfrin scripts

--- src/hirad/interpolate-batches.sh
+++ src/hirad/interpolate-batches.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-for cfgfile in $(ls src/hirad/input_data/configs/era-all-2020*.yaml);
+#clariden
+#for cfgfile in $(ls src/hirad/input_data/configs/era-all-2020*.yaml);
+#do
+#    cmd="sbatch -A a161 -t 12:00:00 -n 1 -c 1 --environment=modulus_env src/hirad/interpolate.sh ${cfgfile}"
+#    echo $cmd
+#    $cmd
+#done
+
+# balfrin
+for cfgfile in $(ls src/hirad/input_data/configs/era-all-2016*.yaml);
 do
-    cmd="sbatch -A a161 -t 12:00:00 -n 1 -c 1 --environment=modulus_env src/hirad/interpolate.sh ${cfgfile}"
+    cmd="sbatch -p postproc -t 12:00:00 -n 1 -c 1 src/hirad/interpolate.sh ${cfgfile}"
     echo $cmd
     $cmd
 done
--- src/hirad/interpolate.sh
+++ src/hirad/interpolate.sh
@@ -8,16 +8,21 @@
 #    pip install anemoi.datasets
 #    python src/hirad/input_data/interpolate_basic.py src/hirad/input_data/era-all.yaml src/hirad/input_data/cosmo-all.yaml /capstor/scratch/cscs/mmcgloho/datasets/processed/era5-cosmo-1h-all-channels/
 #" 
-pip install -e . --no-dependencies
-pip install anemoi.datasets
+#pip install -e . --no-dependencies
+#pip install anemoi.datasets
 #pip install meteodata-lab
 #python src/hirad/input_data/interpolate_realch1.py \
 #    src/hirad/input_data/era.yaml \
 #    /capstor/store/mch/msopr/hirad-gen/basic-torch/era5-realch1/v0.2/info/realch1-lat-lon \
 #    /capstor/store/mch/msopr/hirad-gen/copernicus-datasets/tp-2023-2024.nc \
 #    /capstor/scratch/cscs/mmcgloho/basic-torch/era5-realch1/v1.0/
+#python src/hirad/input_data/interpolate_basic.py \
+#    $1 \
+#    /capstor/scratch/cscs/mmcgloho/basic-torch/era5-cosmo-1h-all-channels/info/cosmo-lat-lon \
+#    /capstor/scratch/cscs/mmcgloho/basic-torch/era5-cosmo-1h-all-channels/
+
 python src/hirad/input_data/interpolate_basic.py \
-#    src/hirad/input_data/era-all.yaml \ 
     $1 \
-    /capstor/scratch/cscs/mmcgloho/basic-torch/era5-cosmo-1h-all-channels/info/cosmo-lat-lon \
-    /capstor/scratch/cscs/mmcgloho/basic-torch/era5-cosmo-1h-all-channels/
+    /store_new/mch/msopr/hirad-gen/basic-torch/era5-cosmo-1h-all-channels/info/cosmo-lat-lon \
+    /store_new/mch/msopr/hirad-gen/basic-torch/era5-cosmo-1h-all-channels/
+
