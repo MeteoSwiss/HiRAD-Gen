a2245f1207f277ca042ba34e1a71f53001aebdf9
Mary McGlohon <Mary.McGlohon@meteoswiss.ch>
2025-11-20 09:53:03 +0100

    add interpolate script

---
+++ src/hirad/interpolate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --time=12:00:00
+
+
+#srun -A a161 -t 12:00:00 --environment=modulus_env bash -c "
+#    pip install -e . --no-dependencies
+#    pip install anemoi.datasets
+#    python src/hirad/input_data/interpolate_basic.py src/hirad/input_data/era-all.yaml src/hirad/input_data/cosmo-all.yaml /capstor/scratch/cscs/mmcgloho/datasets/processed/era5-cosmo-1h-all-channels/
+#" 
+pip install -e . --no-dependencies
+pip install anemoi.datasets
+#pip install meteodata-lab
+#python src/hirad/input_data/interpolate_realch1.py \
+#    src/hirad/input_data/era.yaml \
+#    /capstor/store/mch/msopr/hirad-gen/basic-torch/era5-realch1/v0.2/info/realch1-lat-lon \
+#    /capstor/store/mch/msopr/hirad-gen/copernicus-datasets/tp-2023-2024.nc \
+#    /capstor/scratch/cscs/mmcgloho/basic-torch/era5-realch1/v1.0/
+python src/hirad/input_data/interpolate_basic.py \
+#    src/hirad/input_data/era-all.yaml \ 
+    $1 \
+    /capstor/scratch/cscs/mmcgloho/basic-torch/era5-cosmo-1h-all-channels/info/cosmo-lat-lon \
+    /capstor/scratch/cscs/mmcgloho/basic-torch/era5-cosmo-1h-all-channels/
