e14ec7853acf7ec161a4c875da0830a2d068fc33
Mary McGlohon <Mary.McGlohon@meteoswiss.ch>
2026-03-03 08:56:15 +0100

    obsolete script

--- src/hirad/utils/generate_grib.py
+++
@@ -1,5 +0,0 @@
-from hirad.utils.inference_utils import save_as_grib
-import torch
-
-target=torch.load('/capstor/scratch/cscs/pstamenk/outputs/generation/generation_era_cosmo_anemoi_JJA_2020/20200601-0000/20200601-0000-target',weights_only=False)
-save_as_grib('myfile.grib','/users/mmcgloho/evalml/resources/inference/templates', ['2t', '10u', '10v', 'tp'], target, 'co2')
