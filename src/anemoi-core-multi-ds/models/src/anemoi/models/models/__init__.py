56ab7b271c0427e4f37b78f7be6945e13cbf7473
Mary McGlohon <Mary.McGlohon@meteoswiss.ch>
2026-03-19 15:57:30 +0100

    copy of anemoi-core-multi-ds for local dev

---
+++ src/anemoi-core-multi-ds/models/src/anemoi/models/models/__init__.py
@@ -0,0 +1,36 @@
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+from .base import BaseGraphModel
+from .diffusiondownscaler_encoder_processor_decoder import AnemoiD2ModelEncProcDec
+from .encoder_processor_decoder import AnemoiModelEncProcDec
+from .ens_encoder_processor_decoder import AnemoiEnsModelEncProcDec
+from .hierarchical import AnemoiModelEncProcDecHierarchical
+from .interpolator import AnemoiModelEncProcDecInterpolator
+from .interpolator import AnemoiModelEncProcDecMultiOutInterpolator
+
+from .autoencoder import AnemoiModelAutoEncoder  # isort: skip
+from .hierarchical_autoencoder import AnemoiModelHierarchicalAutoEncoder  # isort: skip
+from .diffusion_encoder_processor_decoder import AnemoiDiffusionModelEncProcDec  # isort: skip
+from .diffusion_encoder_processor_decoder import AnemoiDiffusionTendModelEncProcDec  # isort: skip
+
+__all__ = [
+    "BaseGraphModel",
+    "AnemoiModelEncProcDec",
+    "AnemoiEnsModelEncProcDec",
+    "AnemoiDiffusionModelEncProcDec",
+    "AnemoiDiffusionTendModelEncProcDec",
+    "AnemoiModelEncProcDecHierarchical",
+    "AnemoiModelEncProcDecInterpolator",
+    "AnemoiEnsModelEncProcDec",
+    "AnemoiModelAutoEncoder",
+    "AnemoiModelHierarchicalAutoEncoder",
+    "AnemoiModelEncProcDecMultiOutInterpolator",
+    "AnemoiD2ModelEncProcDec",
+]
