d4d954371486fd047803d12a8ae3cc41eaeb00c9
Mary McGlohon <marymcglo@gmail.com>
2026-03-31 18:03:33 +0000

    edit configs for example ERA5-CERRA downscaling on ATOS

---
+++ src/anemoi-core-ds-collab/training/train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /hpcperm/che8066/envs/anemoi-downscaling/bin/activate
+
+#SBATCH --qos=ng
+#SBATCH --gpus=1
+
+
+SLURM_GPUS_PER_NODE=1
+SLURM_NNODES=1
+
+anemoi-training train --config-name=sample_training_config
