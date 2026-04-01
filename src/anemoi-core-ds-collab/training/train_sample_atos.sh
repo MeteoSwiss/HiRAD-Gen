8b32d10a09aca93bd9e7fc23683e04858e533831
Mary McGlohon <marymcglo@gmail.com>
2026-04-01 15:30:30 +0000

    rename as atos

--- src/anemoi-core-ds-collab/training/train_sample_atos.sh
+++ src/anemoi-core-ds-collab/training/train_sample_atos.sh
@@ -9,4 +9,4 @@ source /hpcperm/che8066/envs/anemoi-downscaling/bin/activate
 SLURM_GPUS_PER_NODE=1
 SLURM_NNODES=1
 
-anemoi-training train --config-name=sample_training_config
+anemoi-training train --config-name=sample_training_config_atos
