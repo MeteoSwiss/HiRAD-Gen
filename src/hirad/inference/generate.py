ead95395c29afb2319948af782e20a86e492bf96
Petar Stamenkovic <petar.stamenkovic@meteoswiss.ch>
2026-04-02 17:19:22 +0200

    add torch inference mode context

--- src/hirad/inference/generate.py
+++ src/hirad/inference/generate.py
@@ -42,8 +42,8 @@ def main(cfg: DictConfig) -> None:
     if dist.world_size > 1:
         torch.distributed.barrier()
 
-    use_apex_gn = cfg.generation.perf.get("use_apex_gn", False)
-    input_dtype = torch.float16 if cfg.generation.perf.get("force_fp16", False) and use_apex_gn else torch.float32
+    # Set precision for inference
+    input_dtype = torch.float16 if cfg.generation.perf.get("force_fp16", False) else torch.float32
 
     # Parse the inference input times
     if cfg.generation.get("times_range", None) and cfg.generation.get("times", None):
@@ -65,6 +65,7 @@ def main(cfg: DictConfig) -> None:
         dataset_cfg=dataset_cfg, times=times, has_lead_time=has_lead_time
     )
     dataset.stats_to_torch(device=dist.device, dtype=input_dtype)
+    dataset.interpolator.to(device=dist.device)
     is_real_target = dataset_cfg.get("type").split("_")[-1] == "real"
     if is_real_target:
         dataset.regrid_indices_real = dataset.regrid_indices_real.to(dist.device)
@@ -72,6 +73,8 @@ def main(cfg: DictConfig) -> None:
     img_shape = dataset.image_shape()
     img_out_channels = len(dataset.output_channels())
 
+
+    #TODO: Isolate loading into the method of generator
     # Parse the inference mode
     if cfg.generation.inference_mode == "regression":
         load_net_reg, load_net_res = True, False
@@ -92,6 +95,10 @@ def main(cfg: DictConfig) -> None:
             raise FileNotFoundError(f"Missing config file at '{diffusion_model_args_path}'.")
         with open(diffusion_model_args_path, 'r') as f:
             diffusion_model_args = json.load(f)
+        # Disable AMP for inference (even if model is trained with AMP)
+        if "amp_mode" in diffusion_model_args:
+            diffusion_model_args["amp_mode"] = False
+        use_apex_gn = diffusion_model_args.get("use_apex_gn", False)
 
         net_res = EDMPrecondSuperResolution(**diffusion_model_args)
 
@@ -101,13 +108,11 @@ def main(cfg: DictConfig) -> None:
             device=dist.device
         )
         
-        net_res = net_res.eval().to(device).to(memory_format=torch.channels_last)
+        net_res = net_res.eval().to(device)
+        if use_apex_gn:
+            net_res = net_res.to(memory_format=torch.channels_last)
         if cfg.generation.perf.force_fp16:
             net_res.use_fp16 = True
-
-        # Disable AMP for inference (even if model is trained with AMP)
-        if hasattr(net_res, "amp_mode"):
-            net_res.amp_mode = False
     else:
         net_res = None
 
@@ -122,6 +127,10 @@ def main(cfg: DictConfig) -> None:
             raise FileNotFoundError(f"Missing config file at '{regression_model_args_path}'.")
         with open(regression_model_args_path, 'r') as f:
             regression_model_args = json.load(f)
+        # Disable AMP for inference (even if model is trained with AMP)
+        if "amp_mode" in regression_model_args:
+            regression_model_args["amp_mode"] = False
+        use_apex_gn_reg = regression_model_args.get("use_apex_gn", False)
 
         net_reg = UNet(**regression_model_args)
 
@@ -131,24 +140,25 @@ def main(cfg: DictConfig) -> None:
             device=dist.device
         )
         
-        net_reg = net_reg.eval().to(device).to(memory_format=torch.channels_last)
+        net_reg = net_reg.eval().to(device)
+        if use_apex_gn_reg:
+            net_reg = net_reg.to(memory_format=torch.channels_last)
         if cfg.generation.perf.force_fp16:
             net_reg.use_fp16 = True
-
-        # Disable AMP for inference (even if model is trained with AMP)
-        if hasattr(net_reg, "amp_mode"):
-            net_reg.amp_mode = False
     else:
         net_reg = None
 
     # Reset since we are using a different mode.
     if cfg.generation.perf.use_torch_compile:
+        torch._dynamo.config.cache_size_limit = 264
         torch._dynamo.reset()
-        # Only compile residual network
-        # Overhead of compiling regression network outweights any benefits
         if net_res:
-            net_res = torch.compile(net_res) #, mode="reduce-overhead")
-        # removed reduce-overhead because it was breaking cuda graph compilation
+            net_res = torch.compile(net_res)
+        if net_reg:
+            net_reg = torch.compile(net_reg)
+
+
+
     generator = Generator(
         net_reg=net_reg,
         net_res=net_res,
@@ -186,149 +196,152 @@ def main(cfg: DictConfig) -> None:
 
     torch_cuda_profiler = (
         torch.cuda.profiler.profile()
-        if torch.cuda.is_available()
+        if torch.cuda.is_available() and cfg.generation.perf.get("profile", False)
         else contextlib.nullcontext()
     )
     torch_nvtx_profiler = (
         torch.autograd.profiler.emit_nvtx()
-        if torch.cuda.is_available()
+        if torch.cuda.is_available() and cfg.generation.perf.get("profile", False)
         else contextlib.nullcontext()
     )
     with torch_cuda_profiler:
         with torch_nvtx_profiler:
+            with torch.inference_mode():
 
-            data_loader = torch.utils.data.DataLoader(
-                dataset=dataset, sampler=sampler, batch_size=1, pin_memory=True
-            )
-            time_index = -1
-            if dist.rank == 0:
-                writer_executor = ThreadPoolExecutor(
-                    max_workers=cfg.generation.perf.num_writer_workers
+                data_loader = torch.utils.data.DataLoader(
+                    dataset=dataset, sampler=sampler, batch_size=1, pin_memory=True
                 )
-                writer_threads = []
-
-            # Create timer objects only if CUDA is available
-            use_cuda_timing = torch.cuda.is_available()
-            if use_cuda_timing:
-                start = torch.cuda.Event(enable_timing=True)
-                end = torch.cuda.Event(enable_timing=True)
-            else:
-                # Dummy no-op functions for CPU case
-                class DummyEvent:
-                    def record(self):
-                        pass
-
-                    def synchronize(self):
-                        pass
-
-                    def elapsed_time(self, _):
-                        return 0
-
-                start = end = DummyEvent()
-
-            dataset.interpolator.to_torch(device=dist.device)
-
-            static_channels = dataset.get_static_data()
-            if static_channels is not None:
-                static_channels = static_channels[None, ::].flip(-2)
-                if use_apex_gn:
-                    static_channels = static_channels.to(
-                        dist.device,
-                        dtype=input_dtype,
-                        non_blocking=True,
-                    ).to(memory_format=torch.channels_last)
-                else:
-                    static_channels = (
-                        static_channels.to(dist.device)
-                        .to(input_dtype)
-                        .contiguous()
-                    )
-            lead_time_label = None
-
-            times = dataset.time()
-            for index, (image_tar, image_lr, *date_str) in enumerate(
-                iter(data_loader)
-            ):
-                time_index += 1
+                time_index = -1
                 if dist.rank == 0:
-                    logger0.info(f"starting index: {time_index} time: {times[sampler[time_index]]}")
-
-                if time_index == warmup_steps:
-                    start.record()
-
-                savedir = os.path.join(output_path,f"{times[sampler[time_index]]}")
-                os.makedirs(savedir,exist_ok=True)
-                # continue
-                if is_real_target:
-                    image_tar = regrid_icon_to_rotlatlon(
-                        image_tar.to(dist.device, dtype=input_dtype),
-                        dataset.regrid_indices_real,
-                        dataset.regrid_weights_real,
+                    writer_executor = ThreadPoolExecutor(
+                        max_workers=cfg.generation.perf.num_writer_workers
                     )
-                    if dataset.trim_edge > 0:
-                        image_tar = image_tar[:, :, dataset.trim_edge:-dataset.trim_edge, dataset.trim_edge:-dataset.trim_edge]
-                else:
-                    image_tar = image_tar.reshape(*image_tar.shape[:-1], *dataset.image_shape())
-                if lead_time_label:
-                    lead_time_label = lead_time_label[0].to(dist.device).contiguous()
-                else:
-                    lead_time_label = None
-                image_lr = dataset.interpolator(image_lr.to(dist.device, dtype=input_dtype)).reshape(*image_lr.shape[:-1], *dataset.image_shape()).flip(-2)
-                image_lr = dataset.normalize_input(image_lr)
-                image_lr = image_lr.to(memory_format=torch.channels_last)
-                random_seed = cfg.generation.get("random_seed", None)+index if cfg.generation.get("randomize", False) and cfg.generation.get("random_seed", None) is not None else None
-                date_embedding = None
-                if dataset._n_month_hour_channels:
-                    date_embedding = dataset.make_time_grids(*date_str, dist.device, dtype=input_dtype)
-                image_out, image_reg = generator.generate(
-                                            image_lr,
-                                            static_channels=static_channels,
-                                            date_embedding=date_embedding,
-                                            lead_time_label=lead_time_label,
-                                            randomize=cfg.generation.get("randomize", False),
-                                            random_seed=random_seed
-                                        )
+                    writer_threads = []
 
-                if dist.rank == 0:
-                    batch_size = image_out.shape[0]
-                    # write out data in a seperate thread so we don't hold up inferencing
-                    image_tar = image_tar[0].squeeze().cpu().numpy()
-                    prediction_ensemble = dataset.denormalize_output(image_out).squeeze().flip(-2).cpu().numpy()
-                    baseline = dataset.denormalize_input(image_lr)[0].squeeze().flip(-2).cpu().numpy()
-                    if image_reg is not None:
-                        mean_pred = dataset.denormalize_output(image_reg)[0].squeeze().flip(-2).cpu().numpy()
-                    writer_threads.append(
-                        writer_executor.submit(
-                            save_results_as_torch,
-                            savedir,
-                            times[sampler[time_index]],
-                            prediction_ensemble,
-                            image_tar,
-                            baseline,
-                            mean_pred if image_reg is not None else None,
+                # Create timer objects only if CUDA is available
+                use_cuda_timing = torch.cuda.is_available()
+                if use_cuda_timing:
+                    start = torch.cuda.Event(enable_timing=True)
+                    end = torch.cuda.Event(enable_timing=True)
+                else:
+                    # Dummy no-op functions for CPU case
+                    class DummyEvent:
+                        def record(self):
+                            pass
+
+                        def synchronize(self):
+                            pass
+
+                        def elapsed_time(self, _):
+                            return 0
+
+                    start = end = DummyEvent()
+
+                #TODO: Isolate static channel loading into the method of generator or reuse training manager static channel loading
+                static_channels = dataset.get_static_data()
+                if static_channels is not None:
+                    static_channels = static_channels[None, ::].flip(-2)
+                    if use_apex_gn:
+                        static_channels = static_channels.to(
+                            dist.device,
+                            dtype=input_dtype,
+                            non_blocking=True,
+                        ).to(memory_format=torch.channels_last)
+                    else:
+                        static_channels = (
+                            static_channels.to(dist.device)
+                            .to(input_dtype)
+                            .contiguous()
                         )
-                    )
-            end.record()
-            end.synchronize()
-            elapsed_time = (
-                start.elapsed_time(end) / 1000.0 if use_cuda_timing else 0
-            )  # Convert ms to s
-            timed_steps = time_index + 1 - warmup_steps
-            if dist.rank == 0 and use_cuda_timing:
-                average_time_per_batch_element = elapsed_time / timed_steps / batch_size
-                logger.info(
-                    f"Total time to run {timed_steps} steps and {batch_size} members = {elapsed_time} s"
-                )
-                logger.info(
-                    f"Average time per batch element = {average_time_per_batch_element} s"
-                )
+                lead_time_label = None
 
-            # make sure all the workers are done writing
-            if dist.rank == 0:
-                for thread in list(writer_threads):
-                    thread.result()
-                    writer_threads.remove(thread)
-                writer_executor.shutdown()
+                times = dataset.time()
+                for index, (image_tar, image_lr, *date_str) in enumerate(
+                    iter(data_loader)
+                ):
+                    time_index += 1
+                    if dist.rank == 0:
+                        logger0.info(f"starting index: {time_index} time: {times[sampler[time_index]]}")
+
+                    if time_index == warmup_steps:
+                        start.record()
+
+                    savedir = os.path.join(output_path,f"{times[sampler[time_index]]}")
+                    os.makedirs(savedir,exist_ok=True)
+                    
+                    #TODO: Move all the data processing inside the generator and just pass raw data to it. This includes regridding, normalization, date embedding creation, etc.
+                    # Same as with static channel loading, we can reuse some of the code from training manager for this. This will also make it easier to maintain and update the data processing steps in one place.
+                    if is_real_target:
+                        image_tar = regrid_icon_to_rotlatlon(
+                            image_tar.to(dist.device, dtype=input_dtype),
+                            dataset.regrid_indices_real,
+                            dataset.regrid_weights_real,
+                        )
+                        if dataset.trim_edge > 0:
+                            image_tar = image_tar[:, :, dataset.trim_edge:-dataset.trim_edge, dataset.trim_edge:-dataset.trim_edge]
+                    else:
+                        image_tar = image_tar.reshape(*image_tar.shape[:-1], *dataset.image_shape())
+                    if lead_time_label:
+                        lead_time_label = lead_time_label[0].to(dist.device).contiguous()
+                    else:
+                        lead_time_label = None
+                    image_lr = dataset.interpolator(image_lr.to(dist.device, dtype=input_dtype)).reshape(*image_lr.shape[:-1], *dataset.image_shape()).flip(-2)
+                    image_lr = dataset.normalize_input(image_lr)
+                    if use_apex_gn:
+                        image_lr = image_lr.to(memory_format=torch.channels_last)
+                    date_embedding = None
+                    if dataset._n_month_hour_channels:
+                        date_embedding = dataset.make_time_grids(*date_str, dist.device, dtype=input_dtype)
+                    random_seed = cfg.generation.get("random_seed", None)+index if cfg.generation.get("randomize", False) and cfg.generation.get("random_seed", None) is not None else None
+                    image_out, image_reg = generator.generate(
+                                                image_lr,
+                                                static_channels=static_channels,
+                                                date_embedding=date_embedding,
+                                                lead_time_label=lead_time_label,
+                                                randomize=cfg.generation.get("randomize", False),
+                                                random_seed=random_seed
+                                            )
+
+                    if dist.rank == 0:
+                        batch_size = image_out.shape[0]
+                        # write out data in a seperate thread so we don't hold up inferencing
+                        image_tar = image_tar[0].squeeze().cpu().numpy()
+                        prediction_ensemble = dataset.denormalize_output(image_out).squeeze().flip(-2).cpu().numpy()
+                        baseline = dataset.denormalize_input(image_lr)[0].squeeze().flip(-2).cpu().numpy()
+                        if image_reg is not None:
+                            mean_pred = dataset.denormalize_output(image_reg)[0].squeeze().flip(-2).cpu().numpy()
+                        writer_threads.append(
+                            writer_executor.submit(
+                                save_results_as_torch,
+                                savedir,
+                                times[sampler[time_index]],
+                                prediction_ensemble,
+                                image_tar,
+                                baseline,
+                                mean_pred if image_reg is not None else None,
+                            )
+                        )
+                end.record()
+                end.synchronize()
+                elapsed_time = (
+                    start.elapsed_time(end) / 1000.0 if use_cuda_timing else 0
+                )  # Convert ms to s
+                timed_steps = time_index + 1 - warmup_steps
+                if dist.rank == 0 and use_cuda_timing:
+                    average_time_per_batch_element = elapsed_time / timed_steps / batch_size
+                    logger.info(
+                        f"Total time to run {timed_steps} steps and {batch_size} members = {elapsed_time} s"
+                    )
+                    logger.info(
+                        f"Average time per batch element = {average_time_per_batch_element} s"
+                    )
+
+                # make sure all the workers are done writing
+                if dist.rank == 0:
+                    for thread in list(writer_threads):
+                        thread.result()
+                        writer_threads.remove(thread)
+                    writer_executor.shutdown()
 
     if dist.rank == 0:
         f.close()
