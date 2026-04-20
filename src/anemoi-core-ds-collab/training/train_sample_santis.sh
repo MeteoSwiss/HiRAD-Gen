3856ee26fbd8f49b613976641ed6b7c95b80085e
Mary McGlohon <Mary.McGlohon@meteoswiss.ch>
2026-04-20 18:40:02 +0200

    gpus_per_node=4 on santis

--- src/anemoi-core-ds-collab/training/train_sample_santis.sh
+++ src/anemoi-core-ds-collab/training/train_sample_santis.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --account=c38
+#SBATCH -t 12:00:00
 
 uenv start pytorch
 source .venv/bin/activate
 
 ANEMOI_BASE_SEED=1
-SLURM_GPUS_PER_NODE=1
+SLURM_GPUS_PER_NODE=4
 SLURM_NNODES=1
 
 anemoi-training train --config-name=sample_training_config_santis
