453d76016bdc950805cd54fef8b940c8190e9db7
David Leutwyler <david.leutwyler@meteoswiss.ch>
2026-05-28 15:23:08 +0200

    plot more percentile in diurnal cycle

---
+++ src/hirad/eval_precip.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH --job-name="eval_precip"
+
+### HARDWARE ###
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=72
+#SBATCH --time=24:00:00
+#SBATCH --no-requeue
+#SBATCH --exclusive
+
+### OUTPUT ###
+#SBATCH --output=./logs/plots_precip_bias.log
+
+### ENVIRONMENT ####
+#SBATCH -A c38
+
+### CONFIG ###
+CONFIG_NAME="src/hirad/conf/eval_real.yaml"
+
+srun --mpi=pmix --network=disable_rdzv_get --environment=./ci/edf/modulus_env.toml bash -c "
+    pip install -e .
+
+    # Diurnal cycle
+    # python src/hirad/eval/diurnal_cycle_precip_mean_wet-hour.py --config-name=${CONFIG_NAME}
+    # python src/hirad/eval/diurnal_cycle_precip_high_percentiles.py --config-name=${CONFIG_NAME}
+
+    # Histograms
+    # python src/hirad/eval/hist.py --config-name=${CONFIG_NAME}
+    # python src/hirad/eval/probability_of_exceedance.py --config-name=${CONFIG_NAME}
+
+    # Maps
+    # python src/hirad/eval/map_precip_stats.py --config-name=${CONFIG_NAME}
+"
